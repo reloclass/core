c1b25770a43bf32a4cb845164162aa97033381df
张文山 <willzhang@hanyunwangxue.com>
2022-01-05 17:58:21 +0800

    【Protocol】0.1.1

---
+++ user/user.go
@@ -0,0 +1,9 @@
+package user
+
+import (
+	`fmt`
+)
+
+func (u *User) LessonCacheKey(lessonId, yunkeId int64) string {
+	return fmt.Sprintf("%d:%d:%d", yunkeId, lessonId, u.Id)
+}
---
+++ user/user.pb.go
@@ -0,0 +1,6 @@
+package user
+
+// User 不要删除，也不要修改
+type User struct {
+	Id int64
+}
