34c89ccbce6e9e920c88e1d2bdeb95ce51a35417
张文山 <willzhang@hanyunwangxue.com>
2022-01-11 17:27:58 +0800

    【Core】0.1.8

---
+++ cloudfile/dir.go
@@ -0,0 +1,13 @@
+package cloudfile
+
+// Dir 文件信息
+type Dir struct {
+	// 学校编码
+	SchoolId int64
+	// 文件ID
+	FileId string
+	// 用户ID
+	UserId int64
+	// 文件类型
+	Type DirType
+}
---
+++ role/id.go
@@ -0,0 +1,9 @@
+package role
+
+type Id int64
+
+const (
+	Admin   Id = 1
+	Teacher Id = 2
+	Student Id = 3
+)
